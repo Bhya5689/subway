469b1b185e564e7bd7409445b1b9114fd425336a
qeg1215 <kjhnml1234@naver.com>
2022-11-19 03:49:00 +0900

    API

--- src/main/java/iducs/capstone/subway/controller/APIController.java
+++ src/main/java/iducs/capstone/subway/controller/APIController.java
@@ -17,9 +17,64 @@ public class APIController {
         this.trainService = trainService;
     }
 
+    @GetMapping("/line1")
+    public List<TrainEntity> trainList1(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1001");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line2")
+    public List<TrainEntity> trainList2(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1002");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line3")
+    public List<TrainEntity> trainList3(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1003");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line4")
+    public List<TrainEntity> trainList4(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1004");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line5")
+    public List<TrainEntity> trainList5(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1005");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line6")
+    public List<TrainEntity> trainList6(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1006");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line7")
+    public List<TrainEntity> trainList7(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1007");
+        System.out.println(entities);
+        return entities;
+    }
+
+    @GetMapping("/line8")
+    public List<TrainEntity> trainList8(){
+        List<TrainEntity> entities = trainService.getTListBySubId("1008");
+        System.out.println(entities);
+        return entities;
+    }
+
     @GetMapping("/line9")
     public List<TrainEntity> trainList(){
-        System.out.println("ddd");
         List<TrainEntity> entities = trainService.getTListBySubId("1009");
         System.out.println(entities);
         return entities;
